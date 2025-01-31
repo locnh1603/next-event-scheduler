a54c70aff8f8550552b261336ec718e1cb685813
Loc Nguyen Huu <nghloc1603@gmail.com>
2025-01-31 21:53:03 +0700

    fix: build issue related to next-auth

--- src/app/api/events/[id]/route.ts
+++ src/app/api/events/[id]/route.ts
@@ -2,14 +2,15 @@ import {auth} from '@/auth';
 import {NextResponse} from 'next/server';
 import dbConnect from '@/app/lib/dbConnect';
 
-export const GET = auth(async (req, { params }) => {
-  const param = await params;
-  if (req.auth && param) {
-    const {id} = param;
+export const GET = async (req: Request, { params }: {params: Promise<{id: string}>}) => {
+  const { id } = await params;
+  const authSession = await auth();
+  if (authSession && id) {
     await dbConnect();
     // TODO : handle get events
     console.log(id);
     return NextResponse.json({ message : 'success'}, {status: 200});
   }
   return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
-})
+}
+
--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,10 +1,9 @@
-import {auth} from '@/auth';
 import {NextResponse} from 'next/server';
 import dbConnect from '@/app/lib/dbConnect';
 import Event from "@/app/models/event.model";
 
-export const GET = auth(async () => {
+export const GET = async () => {
   await dbConnect();
   const events = await Event.find({});
   return NextResponse.json(events);
-})
+}
