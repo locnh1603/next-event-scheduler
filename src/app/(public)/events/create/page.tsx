b63b1546602ce2a61b4131d3a63863f21a61e752
Loc Nguyen Huu <nghloc1603@gmail.com>
2025-02-01 16:02:52 +0700

    fix: fix build issue

--- src/app/(public)/events/create/page.tsx
+++ src/app/(public)/events/create/page.tsx
@@ -3,8 +3,6 @@ import {Box, Typography} from '@mui/material';
 import {useState} from 'react';
 
 const CreateEvent = () => {
-  const [form, setForm] = useState({});
-  console.log(form);
   return (
     <Box>
       <Typography variant="h5">Create Event</Typography>
