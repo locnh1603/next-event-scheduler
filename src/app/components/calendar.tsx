f46f40d69747110250bdc487cf2aea01796a0880
Loc Nguyen Huu <nghloc1603@gmail.com>
2025-02-02 17:42:35 +0700

    fix: version bump for react-day-picker. and UI fix for related component

--- src/app/components/calendar.tsx
+++ src/app/components/calendar.tsx
@@ -1,76 +1,110 @@
-"use client"
+'use client';
 
-import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker } from "react-day-picker"
+import * as React from 'react';
+import { DayFlag, DayPicker, DropdownProps, SelectionState, UI } from 'react-day-picker';
 
-import { cn } from "@/app/lib/utils"
-import { buttonVariants } from "@/app/components/button"
+import { buttonVariants } from '@/app/components/button';
+import { cn } from '@/app/lib/utils';
+import { Select } from '@radix-ui/react-select';
+import { ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon, ChevronUpIcon } from 'lucide-react';
+import { ScrollArea } from './scroll-area';
+import { SelectContent, SelectItem, SelectTrigger, SelectValue } from './select';
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>
+export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
-function Calendar({
-  className,
-  classNames,
-  showOutsideDays = true,
-  ...props
-}: CalendarProps) {
+function Calendar({ className, classNames, showOutsideDays = true, ...props }: CalendarProps) {
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
-      className={cn("p-3", className)}
+      className={cn('p-3', className)}
       classNames={{
-        months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
-        month: "space-y-4",
-        caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
-        nav: "space-x-1 flex items-center",
-        nav_button: cn(
-          buttonVariants({ variant: "outline" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
+        [UI.Months]: 'relative',
+        [UI.Month]: 'space-y-4 ml-0',
+        [UI.MonthCaption]: 'flex justify-center items-center h-7',
+        [UI.CaptionLabel]: 'text-sm font-medium',
+        [UI.PreviousMonthButton]: cn(
+          buttonVariants({ variant: 'outline' }),
+          'absolute left-1 top-0 h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100'
         ),
-        nav_button_previous: "absolute left-1",
-        nav_button_next: "absolute right-1",
-        table: "w-full border-collapse space-y-1",
-        head_row: "flex",
-        head_cell:
-          "text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]",
-        row: "flex w-full mt-2",
-        cell: cn(
-          "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected].day-range-end)]:rounded-r-md",
-          props.mode === "range"
-            ? "[&:has(>.day-range-end)]:rounded-r-md [&:has(>.day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md"
-            : "[&:has([aria-selected])]:rounded-md"
+        [UI.NextMonthButton]: cn(
+          buttonVariants({ variant: 'outline' }),
+          'absolute right-1 top-0 h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100'
         ),
-        day: cn(
-          buttonVariants({ variant: "ghost" }),
-          "h-8 w-8 p-0 font-normal aria-selected:opacity-100"
+        [UI.MonthGrid]: 'w-full border-collapse space-y-1',
+        [UI.Weekdays]: 'flex',
+        [UI.Weekday]: 'text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]',
+        [UI.Week]: 'flex w-full mt-2',
+        [UI.Day]:
+          'h-9 w-9 text-center rounded-md text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
+        [UI.DayButton]: cn(
+          buttonVariants({ variant: 'ghost' }),
+          'h-9 w-9 p-0 font-normal aria-selected:opacity-100 hover:bg-primary hover:text-primary-foreground'
         ),
-        day_range_start: "day-range-start",
-        day_range_end: "day-range-end",
-        day_selected:
-          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-        day_today: "bg-accent text-accent-foreground",
-        day_outside:
-          "day-outside text-muted-foreground aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
-        day_disabled: "text-muted-foreground opacity-50",
-        day_range_middle:
-          "aria-selected:bg-accent aria-selected:text-accent-foreground",
-        day_hidden: "invisible",
+        [UI.Dropdowns]: 'flex items-center gap-1',
+        [SelectionState.range_end]: 'day-range-end',
+        [SelectionState.selected]:
+          'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
+        [SelectionState.range_middle]: 'aria-selected:bg-accent aria-selected:text-accent-foreground',
+        [DayFlag.today]: 'bg-accent text-accent-foreground',
+        [DayFlag.outside]:
+          'day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30',
+        [DayFlag.disabled]: 'text-muted-foreground opacity-50',
+        [DayFlag.hidden]: 'invisible',
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
+        Chevron: ({ ...props }) => <Chevron {...props} />,
+        Dropdown: ({ value, onChange, ...props }: DropdownProps) => {
+          const selected = props.options?.find((child) => child.value === value);
+          const handleChange = (value: string) => {
+            const changeEvent = {
+              target: { value },
+            } as React.ChangeEvent<HTMLSelectElement>;
+            onChange?.(changeEvent);
+          };
+          return (
+            <Select
+              value={value?.toString()}
+              onValueChange={(value) => {
+                handleChange(value);
+              }}
+            >
+              <SelectTrigger className="pr-1.5 focus:ring-0">
+                <SelectValue>{selected?.label}</SelectValue>
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <ScrollArea className="h-80">
+                  {props.options?.map((option, id: number) => (
+                    <SelectItem key={`${option.value}-${id}`} value={option.value?.toString() ?? ''}>
+                      {option.label}
+                    </SelectItem>
+                  ))}
+                </ScrollArea>
+              </SelectContent>
+            </Select>
+          );
+        },
       }}
       {...props}
     />
-  )
+  );
 }
-Calendar.displayName = "Calendar"
 
-export { Calendar }
+const Chevron = ({ orientation = 'left' }) => {
+  switch (orientation) {
+    case 'left':
+      return <ChevronLeftIcon className="h-4 w-4" />;
+    case 'right':
+      return <ChevronRightIcon className="h-4 w-4" />;
+    case 'up':
+      return <ChevronUpIcon className="h-4 w-4" />;
+    case 'down':
+      return <ChevronDownIcon className="h-4 w-4" />;
+    default:
+      return null;
+  }
+};
+
+Calendar.displayName = 'Calendar';
+
+export { Calendar };
