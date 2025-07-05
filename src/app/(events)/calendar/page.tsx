019e1253fe92046c556a1c0e9cad12b48dbafe62
Loc Nguyen Huu <nghloc1603@gmail.com>
2025-07-05 10:24:59 +0700

    fix: remove SEO optimization from protected page

--- src/app/(events)/calendar/page.tsx
+++ src/app/(events)/calendar/page.tsx
@@ -1,29 +1,15 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Event Calendar | Next Event Scheduler',
-  description:
-    'View all upcoming events in a beautiful calendar view. Powered by FullCalendar and Next.js.',
-  openGraph: {
-    title: 'Event Calendar | Next Event Scheduler',
-    description:
-      'View all upcoming events in a beautiful calendar view. Powered by FullCalendar and Next.js.',
-    url: 'https://yourdomain.com/calendar',
-    siteName: 'Next Event Scheduler',
-    type: 'website',
-  },
-};
-
-('use client');
+'use client';
 import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
 const EventCalendar = () => {
   return (
-    <div className="max-w-full">
-      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
-    </div>
+    <>
+      <div className="max-w-full">
+        <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+      </div>
+    </>
   );
 };
 
